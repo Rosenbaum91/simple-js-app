aea362670f9a3051f3ccde0b7e08c98d04e1afb7
Rosenbaum91 <109424012+Rosenbaum91@users.noreply.github.com>
2022-07-27 16:43:57 -0600

    Added Pokemon

--- JS/scripts.js
+++ JS/scripts.js
@@ -1,4 +1,21 @@
-alert('Hello world');
-
-let favoritefood = 'Pizza';
-document.write(favoritefood);
+let pokemonList = [
+	{	name: 'Mew', 
+		height: 0.4,
+		weight: 4, 
+		types: ['Psychic']},
+	
+    {	name: 'Mewtwo',
+		height: 2,
+		weight: 122,
+		types: ['Psychic']},
+	
+    {	name: 'Kangaskhan',
+		height: 2.2,
+		weight: 80,
+		types: ['normal']},
+    
+    {   name: 'Groudon',
+        height: 3.5,
+        weight: 950,
+        types: ['ground']}
+];
