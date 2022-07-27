c68318218d88184d65ad2012b3e7796607d32c95
Rosenbaum91 <109424012+Rosenbaum91@users.noreply.github.com>
2022-07-27 11:16:02 -0600

    Update scripts.js

--- JS/scripts.js
+++ JS/scripts.js
@@ -1,3 +1,40 @@
-alert('Hello world');
-let favoritefood = 'Pizza';
-document.write(favoritefood);
+let pokemonList = [
+    {   
+        name: 'Pikachu',
+        height: 0.4,
+        types: [
+            'ELECTRIC'
+        ]
+    },
+	{
+		name: 'Mew',
+		height: 0.4,
+		types: ['psychic']
+	},
+	{
+		name: 'Mewtwo',
+		height: 2,
+		types: ['psychic']
+	},
+	{
+		name: 'Charizard',
+		height: 17,
+		types: ['fire', 'flying']
+	},
+	{
+		name: 'Dragonite',
+		height: 22,
+		types: ['dragon', 'flying']
+	},
+];
+
+// loop that iterates over pokemonList and writes names and heights
+for (let i = 0; i < pokemonList.length; i++) {
+	if (pokemonList[i].height > 15) {
+		document.write('<p>' + pokemonList[i].name + ' (Height: ' + pokemonList[i].height + ') - Wow, that\'s a big Pokemon!')
+	} else if (pokemonList[i].height <= 15 && pokemonList[i].height > 10) {
+		document.write('<p>' + pokemonList[i].name + ' (Height: ' + pokemonList[i].height + ') - That\'s a normal size.')
+	} else	if (pokemonList[i].height < 10) {
+		document.write('<p>' + pokemonList[i].name + ' (Height: ' + pokemonList[i].height + ') - That\'s a tiny Pokemon!')
+	}
+}
