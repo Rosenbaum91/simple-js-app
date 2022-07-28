d2de9aed93685a2a67457dfe34dc9b969e942a58
Rosenbaum91 <109424012+Rosenbaum91@users.noreply.github.com>
2022-07-28 10:00:24 -0600

    document loop added

--- JS/scripts.js
+++ JS/scripts.js
@@ -19,3 +19,17 @@ let pokemonList = [
         weight: 950,
         types: ['ground']}
 ];
+
+document.write('<div class="pokemonList">');
+for (let i = 0; i < pokemonList.length; i++) {
+    if (pokemonList[i].height > 1.7) {
+        document.write('<div class="pokemon--big">');
+        document.write(pokemonList[i].name + " (height: " + pokemonList[i].height + ")");
+        document.write(' - Wow, that\'s big!');
+    } else {
+        document.write('<div class="pokemon">');
+        document.write(pokemonList[i].name + " (height: " + pokemonList[i].height + ")");
+    }
+    document.write('</div>');
+}
+document.write('</div>');
