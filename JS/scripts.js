6e4d2fac1f9c139802909debb223f225c221c80a
Rosenbaum91 <109424012+Rosenbaum91@users.noreply.github.com>
2022-08-04 12:10:33 -0600

    Update scripts.js
    added forloop

--- JS/scripts.js
+++ JS/scripts.js
@@ -20,16 +20,30 @@ let pokemonList = [
         types: ['ground']}
 ];
 
-document.write('<div class="pokemonList">');
-for (let i = 0; i < pokemonList.length; i++) {
-    if (pokemonList[i].height > 1.7) {
-        document.write('<div class="pokemon--big">');
-        document.write(pokemonList[i].name + " (height: " + pokemonList[i].height + ")");
-        document.write(' - Wow, that\'s big!');
-    } else {
-        document.write('<div class="pokemon">');
-        document.write(pokemonList[i].name + " (height: " + pokemonList[i].height + ")");
+for (let i = 0; i < pokemonList.length; i++) { //conditionals added for weight
+	if (pokemonList[i].weight > 40) { //If Pokemon weighs over 40 declares wow thats big
+		document.write('<p>' + pokemonList[i].name + ' (weight: ' + pokemonList[i].weight + ') - Wow, that\'s big!')
+	} else if (pokemonList[i].weight <= 40 && pokemonList[i].weight > 20) { //If Pokemon is between 20 and 40 declares thats a decent size
+		document.write('<p>' + pokemonList[i].name + ' (weight: ' + pokemonList[i].weight + ') - That\'s a decent size.')
+	} else	if (pokemonList[i].weight < 20) { //if Pokemon weighs under 20 declares awe thats tiny
+		document.write('<p>' + pokemonList[i].name + ' (weight: ' + pokemonList[i].weight + ') - Awe, that\'s tiny!')
+	}}
+
+  // this is for task 1.5 JS
+  pokemonlist.forEach(function (pokemon) {
+    if (pokemon.height < 2 && pokemon.height > 0.1) {
+      document.write(pokemon.name + "This is a small body/ ");
     }
-    document.write('</div>');
-}
-document.write('</div>');
+    else if (pokemon.height < 3) {
+      document.write(pokemon.name + "This is a normal body/ ");
+    } else { document.write(pokemon.name + "This is a big body,wow!/"); }
+  });
+  
+  let pokemon1 = "Mew(Height:0.4)";
+  document.write(pokemon1);
+  let pokemon2 = "MewTwo(Height:2)-wow , this is big";
+  document.write(pokemon2);
+  let pokemon3 = "machop(Height:2.2)";
+  document.write(pokemon3);
+  let pokemon4 = "Groudon(Height:3.5)";
+  document.write(pokemon4);
