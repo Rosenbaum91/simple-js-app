fda9b308e9186cc8e9a71b36927408d578aca47f
Rosenbaum91 <109424012+Rosenbaum91@users.noreply.github.com>
2022-08-13 14:48:03 -0600

    update

--- JS/scripts.js
+++ JS/scripts.js
@@ -1,93 +1,73 @@
 let pokemonRepository = (function () {
-    let pokemonList = [
-      {
-        name: 'Mew',
-        height: 0.4,
-        weight: 4,
-        types: ['Psychic']
-      },
+  let pokemonList = [
+    {
+      name: 'Mew',
+      height: 0.4,
+      weight: 4,
+      types: ['Psychic']
+    },
+    
+    {
+      name: 'Mewtwo',
+      height: 2,
+      weight: 122,
+      types: ['Psychic']
+    },
+    
+    {
+      name: 'Kangaskhan',
+      height: 2.2,
+      weight: 80,
+      types: ['normal']
+    },
       
-      {
-        name: 'Mewtwo',
-        height: 2,
-        weight: 122,
-        types: ['Psychic']
-      },
-      
-      {
-        name: 'Kangaskhan',
-        height: 2.2,
-        weight: 80,
-        types: ['normal']
-      },
-        
-      {
-        name: 'Groudon',
-        height: 3.5,
-        weight: 950,
-        types: ['ground']
-      }
-    ];
-
-
-    function add(pokemon) {   
-      if (
-        typeof pokemon === "object" &&
-        "name" in pokemon &&
-        "detailsUrl" in pokemon
-      ) {
-        pokemonList.push(pokemon);
-      } else {
-        console.log("Pokemon is not correct");
-      }
+    {
+      name: 'Groudon',
+      height: 3.5,
+      weight: 950,
+      types: ['ground']
     }
-  
-    function getAll() {    
-      return pokemonList;  
-    }
-  
-    function addListItem(pokemon) {  
-      let pokemonList = document.querySelector(".pokemon-list");  
-      let pokemonItem = document.createElement("li");
-      pokemonList.classList.add("group-list-item");
-      pokemonList.classList.add("col-sm-4", "col-md-6", "col-lg-12");
-      let buttonItem = document.createElement("button");  
-      buttonItem.classList.add("pokemonButton");
-      buttonItem.innerText = pokemon.name;
-      buttonItem.setAttribute("data-toggle", "modal");
-      buttonItem.setAttribute("data-target", "#pokemon-modal");
-      $(buttonItem).addClass('button-class btn-block btn m1');
-      pokemonItem.appendChild(buttonItem);  
-      pokemonList.appendChild(pokemonItem);  
-      buttonItem.addEventListener("click", function (button) {
-         showDetails(pokemon);
-  
-       });
-     }
-     function loadlist(){
-      if (pokemon.height < 2 && pokemon.height > 0.1) {
-        document.write(`<p>${pokemon.name} This is a small body!</p>`);
-      }
-      else if (pokemon.height < 3) {
-        document.write(`<p>${pokemon.name} This is a normal body!</p>`);
-      } else { document.write(`<p>${pokemon.name} This is a big body,wow!</p>`); }
-     }
-     function showDetails(pokemon) {
-      console.log(pokemon);
+  ];
 
-     }
-     
-    return {
-      add: add,         
-      getAll: getAll,   
-      addListItem: addListItem,  
-      showDetails: showDetails,
-      
-    }
-  })();
-  function loadList()
-  pokemonRepository.loadList().then(function() {
-    pokemonRepository.getAll().forEach(function (pokemon) {
-    });
-      pokemonRepository.addListItem(pokemon);
-    });
+  function getAll() {
+    return pokemonList;
+  }
+
+  function addListItem(pokemon) {  
+    let pokemonList = document.querySelector(".pokemon-list");  
+    let pokemonItem = document.createElement("li");
+    let buttonItem = document.createElement("button");  
+    buttonItem.classList.add("pokemonButton");
+    buttonItem.innerText = pokemon.name;
+    pokemonItem.appendChild(buttonItem);  
+    pokemonList.appendChild(pokemonItem);  
+    buttonItem.addEventListener("click", function (button) {
+       showDetails(pokemon);
+
+     });
+   }
+
+   function showDetails(pokemon) {
+    console.log(pokemon)
+  }
+
+  function add(pokemon) {
+    pokemonList.push(pokemon)
+  }
+
+  return {
+    getAll: getAll,
+    addListItem: addListItem,
+    add: add,
+    showDetails: showDetails,
+  }
+})();
+
+pokemonRepository.getAll().forEach(function (pokemon) {
+  if (pokemon.height < 2 && pokemon.height > 0.1) {
+    document.write(`<p>${pokemon.name} This is a small body!</p>`);
+  }
+  else if (pokemon.height < 3) {
+    document.write(`<p>${pokemon.name} This is a normal body!</p>`);
+  } else { document.write(`<p>${pokemon.name} This is a big body,wow!</p>`); }
+});
